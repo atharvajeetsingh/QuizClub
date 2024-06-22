ace12c05de717cffa6c471e28dfcaa77e69d7958
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-06-22 21:13:58 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():  # put application's code here
+    return render_template("index.html")
+
+@app.route('/members')
+def members():  # put application's code here
+    return render_template("members.html")
+
+@app.route('/nav')
+def nav():  # put application's code here
+    return render_template("nav.html")
+if __name__ == '__main__':
+    app.run()
