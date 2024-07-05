44a50c4c2847097dc413afca1d4b56c65d8c7583
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-07-06 01:36:09 +0530

    wohi nav including

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():  # put application's code here
-    return render_template("nav.html","index.html")
+    return render_template("nav.html"+"index.html")
 
 @app.route('/members')
 def members():  # put application's code here
